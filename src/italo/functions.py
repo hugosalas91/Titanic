d3a217391dd4f970a4033132db8c1ff2554dd635
hugosalas91 <hugosalas91@gmail.com>
2015-09-07 11:21:16 +0200

    Hugo: Esqueleto del proyecto.

---
+++ src/italo/functions.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+
+def cleanData(data):
+    pass
+    
+def exploratoryAnalysis(data):
+    pass
+    
+def newFeatures(data):
+    pass
+
+def selectFeatures(data):
+    pass 
